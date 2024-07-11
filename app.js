205eb1bd70dbd3de92f786d2010ba4cd99254f36
Alleftina <76490477+Alleftina@users.noreply.github.com>
2024-07-11 12:21:39 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
-usercard.appendChild(p);
 
 let titleInput = document.getElementById("title");
 let textInput = document.getElementById("text");
